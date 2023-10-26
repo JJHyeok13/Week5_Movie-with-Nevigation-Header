52fe43bc2fd339dabada25585e2b676c17ff18ed
SnowBlossom <116304804+JJHyeok13@users.noreply.github.com>
2023-10-26 14:22:02 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import "./App.css"
+import Home from "./Components/Pages/Home";
+import Celebrity from "./Components/Pages/Celebrity";
+import Movies from "./Components/Pages/Movies";
+import NotFound from "./Components/Pages/NotFound";
+import TV from "./Components/Pages/TV";
+import Header from "./Components/Header";
+import AdPage from "./Components/AdPage";
+
+
+function App() {
+  return (
+    <div className="root-wrap">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/tv" element={<TV />} />
+          <Route path="/celebrity" element={<Celebrity />} />
+          <Route path="/notfound" element={<NotFound />} />
+          <Route path="/adpage" element={<AdPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
